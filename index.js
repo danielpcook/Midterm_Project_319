870cb0cf380085708a0edea862c8a8c8cea566ff
Daniel Cook <dpcook125@gmail.com>
2024-03-07 10:35:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -29,9 +29,47 @@ function addItem(){
 
     document.getElementById(assignment).appendChild(newItem);
 
-    outputList.appendChild(listItem);
-    document.getElementById("task-name").value = "";
+    hideAddbox();
 
+}
+
+function clearList(){
+    var l1 = document.getElementById("tasklist1");
+    var l2 = document.getElementById("tasklist2");
+    var l3 = document.getElementById("tasklist3");
+    var l4 = document.getElementById("tasklist4");
+    while (l1.hasChildNodes()) {
+        l1.removeChild(l1.firstChild);
+    }
+    while (l2.hasChildNodes()) {
+        l2.removeChild(l2.firstChild);
+    }
+    while (l3.hasChildNodes()) {
+        l3.removeChild(l3.firstChild);
+    }
+    while (l4.hasChildNodes()) {
+        l4.removeChild(l4.firstChild);
+    }
+
+    fetch("./data.JSON")
+    .then(response => response.json())
+    .then(data => {
+    
+    const categories = data.categories;
+
+    let i = 0;
+    categories.forEach(image => {
+      i = i + 1;
+      const imageListElement = document.getElementById('tasklist' + i);
+      const li = document.createElement('li');
+      li.innerHTML = `
+      <p></p>
+        <img src=${image.url} alt="${image.title} Image" width="100">
+        <p><strong>${image.title}</strong> ${image.description}<hr></p>
+        `;
+      imageListElement.appendChild(li);
+    });
+  })
 }
 
 function taskDetails(name,dateCreated,dueDate){
@@ -50,6 +88,12 @@ function revealAddbox(){
       }
 }
 
+function hideAddbox(){
+    var x = document.getElementById("detailPrompt");
+
+    x.style.display = "none";
+}
+
 function getChoice(){
     var choices = document.getElementsByName("question0");
 
@@ -59,6 +103,7 @@ function getChoice(){
         }
       }
 }
+
 
 
 
